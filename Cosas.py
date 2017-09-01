bcd60502603883409e37906999040ace768bb855
CMAREQ <temomareq@gmail.com>
2017-09-01 12:10:50 -0500

    No se que sea Cosas.py D:

---
+++ Cosas.py
@@ -0,0 +1,9 @@
+#Angel Santiago Jaime
+
+x = [1, 2, 3, 5, 7]
+
+for number in x:
+    if number % 2 == 0:
+        print ('SOY un par {0}' .format(number))
+    else:
+        print ('SOY un impar {0}' .format(number))
